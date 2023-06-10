d571ffe807c3132be5b9696baf1312dd1067fcf8
SDMckone <seandmckone@gmail.com>
2023-06-09 20:46:16 -0400

    Create todo item class

---
+++ src/todo-item.js
@@ -0,0 +1,16 @@
+class todoItem {
+
+  constructor(isCompleted, title, description, dueDate, project) {
+    // Indicates todo item completion
+    this.isCompleted = isCompleted;
+    // Title of the todo item
+    this.title = title;
+    // Description for the todo item
+    this.description = description;
+    // Due date for the todo item
+    this.dueDate = dueDate;
+    // Project the todo item belongs to
+    this.project = project;
+  }
+
+}
