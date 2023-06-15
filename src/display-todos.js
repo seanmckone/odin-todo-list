e4302d5e8d3ffe592cb6dc427fe30c9a540b7606
SDMckone <seandmckone@gmail.com>
2023-06-15 13:54:15 -0400

    Update display-todos to display todos properly

--- src/display-todos.js
+++ src/display-todos.js
@@ -4,7 +4,26 @@ function displayTodos(todoList) {
   const todoListElement = document.createElement("div");
   todoListElement.id = "todo-list";
 
-  todoList.forEach(todo => {
+  todoList.forEach(todoItem => {
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
 
+    const completedButton = document.createElement("button");
+    completedButton.classList.add("completed-button")
+    const completedButtonIcon = document.createElement("img");
+    completedButtonIcon.src = "../assets/icons/circle-outline.svg";
+    completedButton.appendChild(completedButtonIcon);
+    todo.appendChild(completedButton);
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.classList.add("todo-title");
+    todoTitle.textContent = todoItem.title;
+    todo.appendChild(todoTitle);
+
+    todoListElement.appendChild(todo);
   });
+
+  return todoListElement;
 }
+
+export default displayTodos;
