ec3c161324c8cde10e848870328e1b0bec959799
SDMckone <seandmckone@gmail.com>
2023-06-22 16:37:18 -0400

    Fix display-todos to display todos from input proj

--- src/display-todos.js
+++ src/display-todos.js
@@ -1,27 +1,29 @@
 // Display all todo items on the DOM
-function displayTodos(todoList) {
+function displayTodos(todoList, project) {
 
   const todoListElement = document.createElement("div");
   todoListElement.id = "todo-list";
 
   todoList.forEach(todoItem => {
-    const todo = document.createElement("div");
-    todo.classList.add("todo");
+    if (todoItem.project === project) {
+      const todo = document.createElement("div");
+      todo.classList.add("todo");
 
-    const completedButton = document.createElement("input");
-    completedButton.type = "checkbox";
-    completedButton.checked = todoItem.isCompleted;
+      const completedButton = document.createElement("input");
+      completedButton.type = "checkbox";
+      completedButton.checked = todoItem.isCompleted;
 
-    completedButton.classList.add("completed-button")
+      completedButton.classList.add("completed-button")
 
-    todo.appendChild(completedButton);
+      todo.appendChild(completedButton);
 
-    const todoTitle = document.createElement("h3");
-    todoTitle.classList.add("todo-title");
-    todoTitle.textContent = todoItem.title;
-    todo.appendChild(todoTitle);
+      const todoTitle = document.createElement("h3");
+      todoTitle.classList.add("todo-title");
+      todoTitle.textContent = todoItem.title;
+      todo.appendChild(todoTitle);
 
-    todoListElement.appendChild(todo);
+      todoListElement.appendChild(todo);
+    }
   });
 
   return todoListElement;
